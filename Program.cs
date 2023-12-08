5d25979bc24291a2b68248805b745ebe7d02ed54
diegocp05 <diegocostapires05@gmail.com>
2023-12-08 11:46:49 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+// WRITELINE , SMARTPHONE, LIGAR, INSTALAR
+
+Console.WriteLine("Smartphone: Nokia");
+Smartphone nokia = new Nokia(numero: "1111", modelo: "ABC", imei: "123", memoria: 333);
+nokia.Ligar();
+nokia.InstalarAplicativo("Ifood");
+
+Console.WriteLine("Smartphone: Iphone");
+Smartphone iphone = new Iphone(numero: "2222", modelo: "CBA", imei: "321", memoria: 666);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Caixa APP");
